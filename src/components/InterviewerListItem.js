7b1e9bd253ab0146266d9ce8c9c8a2416bc64748
Adetola <adetolafadairo@gmail.com>
2022-03-23 15:47:30 -0700

    creating InterviewerListItem files and styling

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss"
+
+export default function InterviewerListItem(props) {
+
+
+  let interClass = classNames("interviewers__item", {
+    "interviewers__item--selected ": props.selected,
+    
+ });
+
+  
+
+  return (
+    <li onClick={() => {props.setInterviewer(props.id)}} className={interClass}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      Sylvia Palmer
+    </li>
+  )
+}
