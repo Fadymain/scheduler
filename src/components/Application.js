0a28dd38224f703e51c48caee1b54f15082dbca5
Adetola <adetolafadairo@gmail.com>
2022-03-26 13:43:31 -0700

    adding useEffect to the Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList";
 import "components/Application.scss";
 import "components/Appointment"
@@ -44,29 +45,21 @@ const appointments = {
   }
 };
 
-console.log("appointment", Object.values(appointments)[0])
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
+
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
+  const [days, setDays] = useState([]);
 
+  useEffect(() => {
+    const daysURL = `http://localhost:8001/api/days`;
+    axios.get(daysURL).then(response => {
+      console.log("response", response.data)
+      setDays([...response.data])
+    })
+  }, []);
 
   return (
     <main className="layout">
