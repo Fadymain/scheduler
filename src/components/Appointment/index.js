e4de0d4f12424d7649b16ead2d66000f01eb3440
Adetola <adetolafadairo@gmail.com>
2022-03-28 18:35:01 -0700

    adding a save function and Status constant to file

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,12 +4,14 @@ import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
 import Form from "./Form"
+import Status from "./Status"
 import useVisualMode from "hooks/useVisualMode"
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -20,6 +22,18 @@ export default function Appointment(props) {
     transition(CREATE);
   }
  
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING)
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW));
+    
+  }
+  console.log("mode", mode);
+
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -33,10 +47,13 @@ export default function Appointment(props) {
       {mode === CREATE && (
         <Form
           interviewers={props.interviewers}
-          onSave={() => console.log("saving")}
+          onSave={save}
           onCancel={back}
 
         />
+      )}
+      {mode === SAVING && (
+        <Status message="SAVING" />
       )}
 
     </article>
